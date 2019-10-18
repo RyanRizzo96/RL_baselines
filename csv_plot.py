8bca7651bc2b6b7c4ddb81940e1681475399552f
Ryan Rizzo <ryanrizzo66@gmail.com>
2019-10-18 09:30:11 +0200

    Update csv_plot.py

--- csv_plot.py
+++ csv_plot.py
@@ -2,7 +2,7 @@ from baselines.common import plot_util as plot
 import matplotlib.pyplot as plt
 import numpy as np
 
-results = plot.load_results('.log/DEEPQ5k')  # pass variable from bash script
+results = plot.load_results('.log/HER10k')  # pass variable from bash script
 print(results)
 r = results[0]
 plt.plot(np.cumsum(r.monitor.l), plot.smooth(r.monitor.r, radius=1))  # radius to smoother
