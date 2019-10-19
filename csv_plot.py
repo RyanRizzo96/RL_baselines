f9749fb82406dd977090a653dc19ed1cf843ea28
Ryan Rizzo <ryanrizzo66@gmail.com>
2019-10-19 09:04:50 +0200

    Update csv_plot.py

--- csv_plot.py
+++ csv_plot.py
@@ -2,10 +2,12 @@ from baselines.common import plot_util as plot
 import matplotlib.pyplot as plt
 import numpy as np
 
-results = plot.load_results('.log/HER10k')  # pass variable from bash script
+OPENAI_LOGDIR=".log/FetchReach/HER5k/trial1"
+
+results = plot.load_results(OPENAI_LOGDIR)  # pass variable from bash script
 print(results)
 r = results[0]
 plt.plot(np.cumsum(r.monitor.l), plot.smooth(r.monitor.r, radius=1))  # radius to smoother
-plt.xlabel('Steps')
+plt.xlabel('Episodes')
 plt.ylabel('Reward')
 plt.show()
