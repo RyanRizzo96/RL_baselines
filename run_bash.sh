5e2ac836e2e0507bb980ad309733e16c2fcbcdad
Ryan Rizzo <ryanrizzo66@gmail.com>
2019-10-19 09:04:52 +0200

    Update run_bash.sh

--- run_bash.sh
+++ run_bash.sh
@@ -4,14 +4,10 @@ source ~/.virtualenvs/baseline_env/bin/activate
 echo $HOME
 echo $PWD
 
-export OPENAI_LOGDIR=.log/HER5k_trial/test4
+export OPENAI_LOGDIR=.log/FetchReach/HER5k/trial1
 export OPENAI_LOG_FORMAT=stdout,log,csv,tensorboard
 
-#tensorboard --logdir ./a2c_cartpole_tensorboard/ --host localhost --port 8088
-
-#Use --env not env for HER. Seems like for other algs this is not the same
-#python3 -m baselines.run --alg=her env=FetchReach-v1 --num_timesteps=1e5
-python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=10000 --save_path=.models/FetchReach_10k_her --play
+python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=5000 --save_path=.models/FetchReach_5k_her
 #python3 -m baselines.run --alg=deepq --env=CartPole-v0 --print_freq=1 --num_timesteps=1e4
 
 python3 csv_plot.py
