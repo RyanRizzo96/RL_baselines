33955457c627f87c9bfac91dab95faa9a9b0767e
Ryan Rizzo <ryanrizzo66@gmail.com>
2019-10-20 08:59:15 +0200

    Update run_bash.sh

--- run_bash.sh
+++ run_bash.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 source ~/.virtualenvs/baseline_env/bin/activate
 
-echo $HOME
-echo $PWD
+# echo $HOME
+# echo $PWD
 
-export OPENAI_LOGDIR=.log/FetchReach/HER5k/trial1
+# export OPENAI_LOGDIR=.log/FetchReach/HER5k/trial1
 export OPENAI_LOG_FORMAT=stdout,log,csv,tensorboard
 
 python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=5000 --save_path=.models/FetchReach_5k_her
-#python3 -m baselines.run --alg=deepq --env=CartPole-v0 --print_freq=1 --num_timesteps=1e4
 
-python3 csv_plot.py
+# python3 csv_plot.py
