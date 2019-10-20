979918ca136dd273c481dfd067c8ffb708d58e0d
Ryan Rizzo <ryanrizzo66@gmail.com>
2019-10-20 20:00:59 +0200

    Trained different models

--- run_bash.sh
+++ run_bash.sh
@@ -7,6 +7,6 @@ source ~/.virtualenvs/baseline_env/bin/activate
 # export OPENAI_LOGDIR=.log/FetchReach/HER5k/trial1
 export OPENAI_LOG_FORMAT=stdout,log,csv,tensorboard
 
-python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=5000 --save_path=.models/FetchReach/5k_512_hidden
+python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=5000 --save_path=.models/FetchReach/QPI_LR_0.01
 
 # python3 csv_plot.py
