e466165e4449731d2cca4dfc3fd774c27a4a5be1
Ryan Rizzo <ryanrizzo66@gmail.com>
2019-10-26 13:05:01 +0200

    Running first test on p3.8xlarge 2e6 timesteps
    python3 mpirun -np 19 python -m baselines.run --num_env=2 --alg=her --env=FetcPickAndPlace-v1 --env=FetchPush-v1 --num_timesteps=2000000 --save_path=~/policies/her/run1/2mil

--- run_bash.sh
+++ run_bash.sh
@@ -9,4 +9,6 @@ export OPENAI_LOG_FORMAT=stdout,log,csv,tensorboard
 
 python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=5000 --save_path=.models/FetchReach/3_layers
 
+python3 mpirun -np 19 python -m baselines.run --num_env=2 --alg=her --env=FetcPickAndPlace-v1 --env=FetchPush-v1 --num_timesteps=2000000 --save_path=~/policies/her/run1/2mil
+
 # python3 csv_plot.py
