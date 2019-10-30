bc9b4ab972e49d2216610b05f9dd94cfdd42d2c2
Ryan Rizzo <ryanrizzo66@gmail.com>
2019-10-30 09:18:37 +0100

    Test 2.1

--- run_bash.sh
+++ run_bash.sh
@@ -17,8 +17,10 @@ export OPENAI_LOG_FORMAT=stdout,log,csv,tensorboard
 
 # python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=5000
 
-for seed in $(seq 0 5); do OPENAI_LOG_FORMAT=csv OPENAI_LOGDIR=$HOME/logs/her_seed/b32-$seed python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=10000 --seed=$seed --nsteps=32; done
+# for seed in $(seq 0 5); do OPENAI_LOG_FORMAT=csv OPENAI_LOGDIR=$HOME/logs/her_seed/b32-$seed python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=10000 --seed=$seed --nsteps=32; done
 
+# TEST 2
+mpirun -np 19 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1  --num_timesteps=1000 --save_path=/policies/her/test2.1/FPAP_1k --log_path=/logs/her/test2.1/FPAP_1k
 
 # mpirun -np 19 python3 -m baselines.run --num_env=2 --alg=her --env=FetcPickAndPlace-v1 --env=FetchPush-v1 --num_timesteps=2000000 --save_path=~/policies/her/run1/2mil
 
