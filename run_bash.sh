992e986c294f56f25ae3a669c31c2dfbad36bf1f
Ryan Rizzo <ryanrizzo66@gmail.com>
2019-10-31 17:58:44 +0100

    Update run_bash.sh

--- run_bash.sh
+++ run_bash.sh
@@ -13,7 +13,7 @@ export OPENAI_LOG_FORMAT=stdout,log,csv,tensorboard
 
 # mpirun -np 19 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --env=FetchPush-v1 --num_timesteps=10000 --save_path=~/policies/her/run4/10k
 
-# mpirun -np 19 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1  --num_timesteps=1000000 --save_path=./models/her/run5/FPAP_1mil
+python3 -m baselines.run --alg=her --env=FetchReach-v1  --num_timesteps=5000
 
 # python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=0 --load_path=/Users/ryanr/B.Eng/MCAST_Degree_4/Thesis/code/baseline_code/baselines/.models/her/ec2_test_5k_test --play
 
@@ -22,7 +22,7 @@ export OPENAI_LOG_FORMAT=stdout,log,csv,tensorboard
 # for seed in $(seq 0 5); do OPENAI_LOG_FORMAT=csv OPENAI_LOGDIR=$HOME/logs/her_seed/b32-$seed python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=10000 --seed=$seed --nsteps=32; done
 
 # TEST 2
-mpirun -np 19 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1  --num_timesteps=1000000 --save_path=.models/her/FPAP_1mil_run3
+# mpirun -np 19 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1  --num_timesteps=1000000 --save_path=.models/her/FPAP_1mil_run3
 
 # mpirun -np 19 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1  --num_timesteps=1000 --save_path=.models/her/ec2_test_5k_test
 
