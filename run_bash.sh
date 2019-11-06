5804880868beabd8b795fab789dcdca0ef038162
Ryan Rizzo <ryanrizzo66@gmail.com>
2019-11-06 09:35:52 +0100

    test Policy run1.1

--- run_bash.sh
+++ run_bash.sh
@@ -26,6 +26,6 @@ export OPENAI_LOG_FORMAT=stdout,log,csv,tensorboard
 
 # mpirun -np 19 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1  --num_timesteps=1000 --save_path=.models/her/ec2_test_5k_test
 
-mpirun -np 19 python3 -m baselines.run --num_env=2 --alg=her --env=FetchPickAndPlace-v1 --env=FetchPush-v1 --num_timesteps=10000 --save_path=/policies/her/env2/run2
+mpirun -np 19 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=10000 --save_path=/.policies/her/testPolicy/run1
 
 # python3 csv_plot.py
