41967743b06d590e65f768a7c43728714330906c
Ryan Rizzo <ryanrizzo66@gmail.com>
2019-11-25 19:34:59 +0100

    np 16

--- run_bash.sh
+++ run_bash.sh
@@ -23,7 +23,7 @@ export OPENAI_LOG_FORMAT=stdout,log,csv,tensorboard
 
 # mpirun -np 19 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1  --num_timesteps=1000 --save_path=.models/her/ec2_test_5k_test
 
-mpirun -np 19 python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=5000 --save_path=policies/new_env/FSLIDE_200k/standard
+mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=5000 --save_path=policies/new_env/FSLIDE_200k/standard
 
 # mpirun -np 19 python3 -m baselines.run --num_env=5 --alg=her --env=FetchReach-v1 --num_timesteps=5000
 
