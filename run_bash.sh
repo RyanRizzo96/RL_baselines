f60ad53087850e202d4eec609e73e6adfe088f1b
Ryan Rizzo <ryanrizzo66@gmail.com>
2020-02-20 08:15:19 +0100

    Fetch Push 5 seeds 200k

--- run_bash.sh
+++ run_bash.sh
@@ -6,7 +6,7 @@ echo $HOME
 # echo $PWD
 
 export OPENAI_LOG_FORMAT=stdout,log,csv,tensorboard
-
+# PYTHONHUNTERCONFIG="stdlib=False,force_colors=True,source__contains="train
 # python3 -m baselines.run --alg=her --env=FetchReach-v1  --num_timesteps=5000
 
 #coverage run -m baselines.run --alg=her --env=FetchReach-v1  --num_timesteps=5000
@@ -15,12 +15,13 @@ export OPENAI_LOG_FORMAT=stdout,log,csv,tensorboard
 
 # python3 -m baselines.run --alg=her --env=FetchPush-v1 --num_timesteps=0 --load_path=/Users/ryanr/B.Eng/MCAST_Degree_4/Thesis/code/baseline_code/baselines/policies/her/FPUSH_200k/standard --play
 
-# for seed in $(seq 0 2); do OPENAI_LOGDIR=/home/ubuntu/RL_baselines/.log/three_seeds/FPAP_200k-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=200000 --seed=$seed --layers=3; done
-for seed in $(seq 4 5); do OPENAI_LOGDIR=/home/ubuntu/RL_baselines/.log/two_seeds/run_1/FPAP_200k-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=200000 --seed=$seed --layers=3; done
+for seed in $(seq 0 4); do OPENAI_LOGDIR=/home/ubuntu/RL_baselines/.log/five_seeds/FPUSH_200k-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPush-v1 --num_timesteps=200000 --seed=$seed --layers=3; done
+# for seed in $(seq 4 5); do OPENAI_LOGDIR=/home/ubuntu/RL_baselines/.log/two_seeds/run_1/FPAP_200k-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1 --num_timesteps=200000 --seed=$seed --layers=3; done
 # for seed in $(seq 0 5); do OPENAI_LOGDIR=/home/ubuntu/RL_baselines/.log/run_1/layers_6-$seed mpirun -np 16 python3 -m baselines.run --alg=her --env=FetchPush-v1 --num_timesteps=10000 --seed=$seed --layers=6; done
 
-# python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=5000
+# python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=5000 --log_path=.log/tensorboard_test/actor_critic_loss/run4
 
+# PYTHONHUNTER= python3 -m baselines.run --alg=her --env=FetchReach-v1 --num_timesteps=500
 # TEST 2
 # mpirun -np 19 python3 -m baselines.run --alg=her --env=FetchPickAndPlace-v1  --num_timesteps=1000000 --save_path=.models/her/FPAP_1mil_run3
 
